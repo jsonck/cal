a467db961cf1223c28cfe6e5590aea3f8fbd777a
jsonck <jason@cartkit.com>
2025-11-16 07:12:18 -0500

    latest

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module Cal
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Tell Zeitwerk to ignore the API directory entirely (we manually load it in routes.rb)
+    Rails.autoloaders.main.ignore("#{root}/app/api")
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
